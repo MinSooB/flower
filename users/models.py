c137643e1fa7cc1c1b1af111fd379d6e82073c37
MinSooB <bae3003@naver.com>
2020-12-25 21:40:06 +0900

    superuser

--- users/models.py
+++ users/models.py
@@ -5,10 +5,12 @@ from django.contrib.auth.models import AbstractUser
 from django.core.mail import send_mail
 from django.utils.html import strip_tags
 from django.template.loader import render_to_string
+from . import managers
 
 class User(AbstractUser):
     email_verified = models.BooleanField(default = False)
     email_secret = models.CharField(max_length=20, default = "", blank = True)
+    objects = managers.CustomUserManager()
 
     def verify_email(self):
         if self.email_verified is False:
