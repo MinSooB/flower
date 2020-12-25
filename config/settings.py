653d3d644713ec3683bd8fb2e7e975d4eb8dcbf9
MinSooB <bae3003@naver.com>
2020-12-25 22:14:57 +0900

    add

--- config/settings.py
+++ config/settings.py
@@ -169,5 +169,5 @@ if not DEBUG:
     AWS_AUTO_CREATE_BUCKET = True
     AWS_DEFAULT_ACL = "public-read"
 
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
