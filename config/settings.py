657c82d0f91626daeff44af6d3a37b31cf5fb75a
MinSooB <bae3003@naver.com>
2020-12-25 15:46:54 +0900

    test

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if DEBUG:
+if DEBUG is False:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
