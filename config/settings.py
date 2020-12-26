29b23f9be04a34ddff900c1a7d51106b6ba4cd61
MinSooB <bae3003@naver.com>
2020-12-26 21:52:36 +0900

    test

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
+DEBUG = True
 
 ALLOWED_HOSTS = [".elasticbeanstalk.com", "flower.eba-jjrzp73z.ap-northeast-2.elasticbeanstalk.com", "127.0.0.1"]
 
